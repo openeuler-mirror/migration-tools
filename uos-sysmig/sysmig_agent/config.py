06c097f5d790c34c5a28412cdf396e7d700eef75
xuezhixin <xuezhixin@uniontech.com>
2022-03-24 18:10:02 +0800

    Update evaluation algorithm
    Change-Id: If51271e272b9aecf9e0686ef559a5ac4c9fed1fb

--- uos-sysmig/sysmig_agent/config.py
+++ uos-sysmig/sysmig_agent/config.py
@@ -4,6 +4,8 @@ new_os = '统信服务器操作系统V20({})'
 c8 = ['1020a', '1021a', '1050a']
 c7 = ['1000c', '1001c', '1002a']
 
+# agent的安装目录
+agent_install = '/usr/lib/uos-sysmig-agent/'
 # 创建和使用日志 报告的文件夹
 AGENT_DIR = '/var/tmp/uos-migration/'
 # 存量替换
@@ -14,12 +16,19 @@ PRE_MIG_DIR_ADD = '/var/tmp/uos-migration/UOS_analysis_report_add'
 MIGRATION_DIR = '/var/tmp/uos-migration/UOS_migration_log'
 # 迁移分析
 MIGRATION_REPORT_DIR = '/var/tmp/uos-migration/UOS_migration_completed_report'
+# 存放abi兼容性检测数据
+abipwd = '/var/tmp/uos-migration/data/exp-rst'
+ABI_INCOMPAT_PATH = abipwd + '/abi-incompat-pkg.txt'
+ABI_COMPAT_PATH = abipwd + '/abi-compat-pkg.txt'
+AppStream = abipwd + '/sysmig_agent/data/AppStream.txt'
+BaseOS = abipwd + '/sysmig_agent/data/BaseOS.txt'
 
 RPMS = '/var/tmp/uos-migration/.rpms'
 MIGRATION_KERNEL = '/var/tmp/uos-migration/kernel'
 # 迁移所使用到的badpackages
 badpackage7 = 'uos-sysmig/sysmig_agent/data/7badpackage.txt'
 badpackage8 = 'uos-sysmig/sysmig_agent/data/8badpackage.txt'
+ignore_abi_check_8 = 'uos-sysmig/sysmig_agent/data/ignore_abi_check8.txt'
 CACHE_SPACE = 10.0
 # abi结果更新数据库
 abi_file = '/var/tmp/uos-migration/data/exp-rst/agent_ABI_check_result.csv'
