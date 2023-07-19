1ab0a0dc8007c23714291fc351916d0738ab6734
lixin <lixinb@uniontech.com>
2023-07-19 15:44:04 +0800

    添加js文件

---
+++ static/js/admin/alert.js
@@ -0,0 +1,26 @@
+window.alert = function (message, title, callbak) {
+    /// <summary>
+    /// 关闭Alert弹出窗
+    /// </summary>
+    (closeAlert = function () { $('#alert-window').remove(); })();
+    var bg = $('<div class="alert-bg"></div>');
+    var wnd = $('<div class="alert-wnd"><div class="alert-head"><center>'
+                + (title || "")
+                + '</center></div><div class="alert-body"><div class="alert-icon"><span></span></div><div class="alert-msg"><p>'
+                + (message || '') + '</p></div></div><div class="alert-footer"><a class="btn btn-small" id="alert-confirm">取消</a><a class="btn1 btn-small" onclick="close_info();">关闭</a></div></div>');
+    window.top.$(document.body).append($('<div id="alert-window"></div>').append(bg).append(wnd).delegate('#alert-confirm', 'click', function () { closeAlert(); callbak && callbak(); }));
+};
+
+function close_info() {
+    var close_tool_data_json={"mod":"close_tool"};
+    close_tool_data_json.ip="tasks"
+    $.ajax({
+        async:true,
+        type: 'post',
+        dataType: 'json',
+        url: 'MT_close_tool',
+        data: $.toJSON(close_tool_data_json),
+        success: function(res) {
+        }
+    });	//ajax end
+}
