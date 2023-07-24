7711ba42aef49d0ff741c3005ce677bd85909d7d
lixin <lixinb@uniontech.com>
2023-07-24 11:01:06 +0800

    跳转环境检测的路由

--- index.py
+++ index.py
@@ -34,5 +34,14 @@ def MT_note():
     return render_template('MT_note.html')
 
 
+@app.route('/MT_check_env', methods=['GET', 'POST'])
+def MT_check_env():
+    """
+    跳转系统环境检查界面
+    :return:
+    """
+    return render_template('MT_check_evn.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port='9999')
