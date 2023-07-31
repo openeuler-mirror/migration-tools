78da0bd7d63824d5883f8d6190514126e2d0fb9c
lixin <lixinb@uniontech.com>
2023-07-31 10:10:45 +0800

    添加存储空间检测前端接口

--- index.py
+++ index.py
@@ -6,6 +6,18 @@ import sys
 from flask import Flask, render_template, url_for, request, redirect, make_response, session, Response
 app = Flask(__name__)
 
+mods = {'check_storage' : check_storage}
+
+
+def check_methods():
+    if request.method == 'POST':
+        data = request.get_data()
+        json_data = json.loads(data)
+        mod = mods.get(json_data['mod'])
+        if mod:
+            response_str = mod(data)
+            return response_str
+
 
 @app.route('/', methods=['GET', 'POST'])
 def MT_index():
@@ -40,6 +52,19 @@ def MT_check_env():
     跳转系统环境检查界面
     :return:
     """
+    return render_template('MT_check_evn.html')
+
+
+@app.route('/MT_check_storage', methods=['GET', 'POST'])
+def MT_check_storage():
+    """
+    检测/var/cache空间大小
+    :return:
+    """
+    mod = check_methods()
+    if mod:
+        return Response(mod, content_type='application/json')
+
     return render_template('MT_check_evn.html')
 
 
