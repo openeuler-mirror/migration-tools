f9cd5557c5dbb1292cb6895d3da47d54af65b40d
xuezhixin <xuezhixin@uniontech.com>
2023-11-10 15:15:58 +0800

    添加检测系统基本信息并上传到数据库

--- sysmig_agent/short_task.py
+++ sysmig_agent/short_task.py
@@ -103,3 +103,38 @@ def check_kernel(data):
     sql_task_statue(statue, task_id)
     post_server('task_close', task_id)
     return 's'
+
+
+
+def check_info(data):
+    task_id = json.loads(data).get('task_id')
+    # 更新SQL任务状态
+    statue = 1
+    sql_task_statue(statue, task_id)
+    # 发送消息给Server更新任务流状态
+    post_server('task_start', task_id)
+    # 获取agent系统类型
+    agent_os = get_agent_os()
+    # agent storage
+    agent_storage = os_storage()
+    agent_ip = get_local_ip()
+    sql = "UPDATE agent_info SET hostname = '{}', agent_os ='{}', agent_arch = '{}' ,agent_storage = {} ," \
+          "agent_online_status = {} WHERE agent_ip = '{}';".format(platform.node(), agent_os, platform.machine(),
+                                                                   agent_storage, 0, agent_ip)
+    try:
+        DBHelper().execute(sql)
+        statue = 2
+    except:
+        statue = 3
+        sql_task_statue(statue, task_id)
+    # 更新SQL任务状态
+    sql_task_statue(statue, task_id)
+    post_server('task_close', task_id)
+    return 'success'
+
+def get_agent_os():
+    os_version_ret = platform.dist()
+    version = os_version_ret[1].split('.', -1)
+    AGENT_OS = os_version_ret[0] + version[0]
+    return AGENT_OS
+
