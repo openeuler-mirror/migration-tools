c23609be2fe4d20b0527b33e9958324785fbb113
xuezhixin <xuezhixin@uniontech.com>
2023-11-14 11:27:02 +0800

    升级迁移主函数

--- sysmig_agent/centos82uos.py
+++ sysmig_agent/centos82uos.py
@@ -99,23 +99,6 @@ def get_disk_info(string):
     return dev_name, part_num
 
 
-def process_special_pkgs():
-    print("swap *-logos related packages with UniontechOS packages")
-    subprocess.run('rpm -q centos-logos-ipa && dnf swap -y centos-logos-ipa uos-logos-ipa', shell=True)
-    subprocess.run('rpm -q centos-logos-httpd && dnf swap -y centos-logos-httpd uos-logos-httpd', shell=True)				
-    print("redhat-lsb is replaced by system-lsb on UniontechOS")
-    subprocess.run('rpm -q redhat-lsb-core && dnf swap -y redhat-lsb-core system-lsb-core', shell=True)
-    subprocess.run('rpm -q redhat-lsb-submod-security && dnf swap -y redhat-lsb-submod-security system-lsb-submod-security',shell=True)
-    print("rhn related packages is not provided by UniontechOS")
-    subprocess.run('rpm -q rhn-client-tools && dnf -y remove rhn-client-tools python3-rhn-client-tools python3-rhnlib', shell=True)		
-    print("subscription-manager related packages is not provided by UniontechOS")
-    subprocess.run('rpm -q subscription-manager && dnf -y remove subscription-manager', shell=True)
-    print("python3-syspurpose is not provided by UniontechOS")
-    subprocess.run('rpm -q python3-syspurpose && dnf -y remove python3-syspurpose', shell=True)
-    print("remove centos gpg-pubkey")
-    subprocess.run('rpm -e $(rpm -q gpg-pubkey --qf "%{NAME}-%{VERSION}-%{RELEASE} %{PACKAGER}\\n" | grep CentOS | awk \'{print $1}\')', shell=True)
-
-
 def pre_system_rpms_info():
  # display rpms info before conversion
     print("Creating a list of RPMs installed before the switch")
@@ -131,13 +114,17 @@ def pre_system_rpms_info():
         if re.match(hostname+'-rpms-(.*)\.log', f):
             print(f)
 
-def centos8_main(osname):
+
+def centos8_main(osname,task_id):
     global reposdir
 
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    #rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
-    log_name = '/var/tmp/uos-migration/UOS_migration_log/log'
+    # rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
+    log_name = '/var/tmp/uos-migration/UOS_migration_log/migration_log.txt'
+    cwd = '/var/tmp/uos-migration/UOS_migration_log'
+    if not os.path.exists(cwd):
+        os.mkdir(cwd)
     logfile = log_name
     fh = logging.FileHandler(logfile, mode='w')
     fh.setLevel(logging.DEBUG)
@@ -145,27 +132,27 @@ def centos8_main(osname):
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
-
     logger.info("Checking if the tool is executed by root user")
     # check if the script is executed by root user
-    if os.geteuid() != 0:
-        logger.info("Please run the tool as root user.")
-        sys.exit(1)
+    # if os.geteuid() != 0:
+    #     logger.info("Please run the tool as root user.")
+    #     sys.exit(1)
     badpackages = get_bad_packages()
     # check required packages
     logger.info('Checking required packages')
-    for pkg in ['rpm','yum','curl']:
+    for pkg in ['rpm', 'yum', 'curl']:
         if not check_pkg(pkg):
-            logger.info("Could not found "+pkg)
+            logger.info("Could not found " + pkg)
             sys.exit(1)
 
-    #sto system rpms info before migration
+    # sto system rpms info before migration
     pre_system_rpms_info()
 
     # check if the os old_version is supported
     logger.info("========= Checking: distribution =========")
     old_version = subprocess.check_output("rpm -q --whatprovides /etc/redhat-release", shell=True)
     old_version = str(old_version, 'utf-8')
+    delect_release = old_version
     old_version = old_version.split('\n')[:-1]
     if len(old_version) == 0:
         logger.info("You appear to be running an unsupported distribution.")
@@ -178,10 +165,10 @@ def centos8_main(osname):
     old_version = old_version[0]
     logger.info(old_version)
     osrelease = osname + '-release'
-    linux_release = osname+'-linux-release'
+    linux_release = osname + '-linux-release'
     if re.match('uos-release', old_version):
         logger.info("You are already using Uniontech.")
-        sys.exit(1)
+        sys.exit(1)  #####get again
     elif re.match(linux_release, old_version):
         subver = old_version.split('-')[3]
     elif re.match(osrelease, old_version):
@@ -189,70 +176,77 @@ def centos8_main(osname):
     else:
         logger.info("Your are using an unsupported distribution.")
         sys.exit(1)
-    if not re.match('8',subver):
+    if re.match('7', subver):
+        logger.info("Your system will be migrated from CentOS7 to UniontechOS")
+    elif re.match('20', subver):
         logger.info("You appear to be running an unsupported distribution.")
         sys.exit(1)
 
     logger.info("========= Checking: required python packages =========")
     if not check_pkg('/usr/libexec/platform-python'):
         logger.info('/usr/libexec/platform-python not found.')
-        sys.exit(1)
-    base_packages=['basesystem','initscripts','uos-logos','plymouth','grub2','grubby']
+    base_packages = ['basesystem', 'initscripts', 'uos-logos', 'plymouth', 'grub2', 'grubby']
 
     logger.info("========= Checking: yum lock ===========")
     if os.path.exists('/var/run/yum.pid'):
         with open('/var/run/yum.pid', 'r') as f:
             pid = f.read()
             f.close()
-            with open('/proc/'+pid+'/comm', 'r') as ff:
+            with open('/proc/' + pid + '/comm', 'r') as ff:
                 comm = ff.read()
-                logger.info('Another app is currently holding the yum lock: '+comm)
-                logger.info('Running as pid: '+pid)
+                logger.info('Another app is currently holding the yum lock: ' + comm)
+                logger.info('Running as pid: ' + pid)
                 logger.info('Please kill it and run the tool again.')
                 ff.close()
-        sys.exit(1)
+            sql_mig_statue('18')
+            #sql_task_statue(3)
+            sql_task_statue(3, task_id)
+            return None
 
     # check dnf
-    if re.match('8\.',subver):
+    if re.match('8\.', subver):
         logger.info("========= Checking: dnf =========")
         logger.info("Identifying dnf modules that are enabled...")
         enabled_modules = str(
-            subprocess.check_output("dnf module list --enabled | grep rhel | awk '{print $1}'", shell=True), 
+            subprocess.check_output("dnf module list --enabled | grep rhel | awk '{print $1}'", shell=True),
             'utf-8')
         enabled_modules = enabled_modules.split('\n')[:-1]
-        unknown_mods=[]
+        unknown_mods = []
         if len(enabled_modules) > 0:
             for mod in enabled_modules:
                 if re.fullmatch('container-tools|llvm-toolset| perl-DBD-SQLite|perl-DBI|satellite-5-client|perl', mod):
-                    subprocess.run('dnf module reset -y '+mod, shell=True)
+                    subprocess.run('dnf module reset -y ' + mod, shell=True)
                 if not re.fullmatch('container-tools|go-toolset|jmc|llvm-toolset|rust-toolset|virt', mod):
                     unknown_mods.append(mod)
             if len(unknown_mods) > 0:
                 logger.info('This tool is unable to automatically switch module(s) ' \
-                + ','.join(unknown_mods) \
-                + ' from a CentOS \'rhel\' stream to an UniontechOS equivalent.'\
-                )
-                opt = input('Do you want to continue and resolve it manually? (Yes or No)\n' )
-                if opt != 'Yes':
-                    sys.exit(1)
-
+                            + ','.join(unknown_mods) \
+                            + ' from a CentOS \'rhel\' stream to an UniontechOS equivalent.' \
+                            )
+                sql_mig_statue('28')
+                sql_task_statue(3)
 
     logger.info("========= Looking for yumdownloader ==========")
-    os.system('yum -y install uos-license-mini license-config ')
+    cmd_license = 'yum -y install uos-license-mini license-config '
+    run_subprocess(cmd_license)
     if not check_pkg('yumdownloader'):
         subprocess.run("yum -y install yum-utils --disablerepo C* || true", shell=True)
+        # if not check_pkg('yumdownloader'):
+        #    clean_and_exit()
 
     logger.info("========= Start converting =========")
-    if re.match(osname+'-release-8', old_version):
-        repos = 'rpm -qa '+osname+'*repos'
+    subprocess.run('rpm -e --nodeps {}'.format(delect_release) , shell=True)
+    if re.match(osname + '-release', old_version):
+        repos = 'rpm -qa ' + osname + '*repos'
         old_version = subprocess.check_output(repos, shell=True)
         old_version = str(old_version, 'utf-8')[:-1]
-        subprocess.run('rpm -e --nodeps ' + old_version , shell=True)
+        subprocess.run('rpm -e --nodeps ' + old_version, shell=True)
 
     logger.info("Downloading uos release package...")
     dst_release = ['uos-release']
     try:
-        stat = subprocess.check_output("yumdownloader "+' '.join(dst_release), shell=True)
+        downdir = ' --destdir /var/tmp/uos-migration '
+        stat = subprocess.check_output("yumdownloader " + downdir + ' '.join(dst_release), shell=True)
         if not check_pkg('yumdownloader'):
             subprocess.run("yum -y install yum-utils --disablerepo C* || true", shell=True)
         if not check_pkg('yumdownloader'):
@@ -263,50 +257,61 @@ def centos8_main(osname):
         logger.info("Are you behind a proxy? If so, make sure the 'http_proxy' environmen")
         logger.info("variable is set with your proxy address.")
         logger.info("An error occurred while attempting to switch this system to Uniontech" + \
-        "and it may be in an unstable/unbootable state. To avoid further issues, " +\
-        "the script has terminated.")
-
+                    "and it may be in an unstable/unbootable state. To avoid further issues, " + \
+                    "the script has terminated.")
 
     logger.info("Switching old release package with Uniontech...")
-    dst_rpms = [s+'*.rpm' for s in dst_release]
-    oldkeys = osname+'-gpg-keys'
+    dst_rpms = [s + '*.rpm' for s in dst_release]
+    oldkeys = osname + '-gpg-keys'
     subprocess.run('rpm -e --nodeps ' + oldkeys, shell=True)
-    subprocess.run('rpm -i --force ' + ' '.join(dst_rpms) + ' --nodeps', shell=True)
+    subprocess.run('rpm -i --force ' + '/var/tmp/uos-migration/'+dst_rpms[0] + ' --nodeps', shell=True)
+    # os.remove(dst_rpms)
     local_disabled_release_repo()
+    # mig_kernel(kernel_version)
     logger.info("Installing base packages for UniontechOS...")
-    cmd='yum shell -y <<EOF\n\
-remove '+ badpackages +'\n\
-install '+ ' '.join(base_packages) + '\n\
+    cmd = 'yum shell -y <<EOF\n\
+remove ' + badpackages + '\n\
+install ' + ' '.join(base_packages) + '\n\
 run\n\
 EOF'
+
+    _, code = run_subprocess(cmd)
+    if code != 0:
+        logger.info("Could not install base packages.Run 'yum distro-sync' to manually install them..{}.".format(code))
+        sql_mig_statue('38')
+        #sql_task_statue(3)
+        sql_task_statue(3, task_id)
+        return None
+    '''
     try:
-        fdout = open("/var/tmp/uos-migration/UOS_migration_log/mig_log.txt",'a')
-        subprocess.run(cmd,stdout=fdout , shell=True)
+        cwd = '/var/tmp/uos-migration/UOS_migration_log'
+        if not os.path.exists(cwd):
+            os.mkdir(cwd)
+
+        fdout = open("/var/tmp/uos-migration/UOS_migration_log/mig_log.txt", 'a')
         fdout.close()
+        # run_cmd2file(cmd)
+        if code != 0:
+            logger.info("Could not install base packages.Run 'yum distro-sync' to manually install them....")
+            sql_mig_statue('28')
+            return None
     except:
         logger.info("Could not install base packages.Run 'yum distro-sync' to manually install them.")
-        sys.exit(1)
-
+    #     sys.exit(1)
+'''
     if os.access('/usr/libexec/plymouth/plymouth-update-initrd', os.X_OK):
         logger.info("Updating initrd...")
-        indexhtml ='rpm -e --nodeps ' + osname+'-indexhtml'
+        indexhtml = 'rpm -e --nodeps ' + osname + '-indexhtml'
         os.system(indexhtml)
-        fdout = open("/var/tmp/uos-migration/UOS_migration_log/mig_log.txt",'a')
-        subprocess.run('/usr/libexec/plymouth/plymouth-update-initrd',stdout=fdout,shell=True)
-        #run_cmd2file('/usr/libexec/plymouth/plymouth-update-initrd')
+        fdout = open("/var/tmp/uos-migration/UOS_migration_log/mig_log.txt", 'a')
+        subprocess.run('/usr/libexec/plymouth/plymouth-update-initrd', stdout=fdout, shell=True)
+        # run_cmd2file('/usr/libexec/plymouth/plymouth-update-initrd')
         fdout.close()
     logger.info("Switch successful. Syncing with UniontechOS repositories.")
-    logger.debug('358')
-
     if subver == '8.3':
         subprocess.run('yum -y downgrade crypto-policies --allowerasing', shell=True)
-    try:
-        fdout = open("/var/tmp/uos-migration/UOS_migration_log/mig_log.txt",'a')
-        subprocess.run('yum -y distro-sync', stdout=fdout ,shell=True)
-        fdout.close()
-    except:
-        logger.info("error distro-sync migration ....")
-    messageState('2')
+    sql_mig_statue('02')
+
 
 os_version_ret = platform.dist()
 osname = os_version_ret[0]
