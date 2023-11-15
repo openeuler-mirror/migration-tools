1d7faf23b4ff7caf46246f173290cc15565cbfc2
lixin <lixinb@uniontech.com>
2023-11-15 10:08:09 +0800

    删除多余代码

--- views/migration.py
+++ views/migration.py
@@ -1,7 +1,5 @@
 # SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
 # SPDX-License-Identifier:   MulanPubL-2.0-or-later
-import os
-
 from connect_sql import DBHelper
 from logger import *
 
@@ -9,7 +7,6 @@ from flask import *
 from client_requests import *
 from sysmig_agent.utils import *
 from interaction import splice_url
-
 
 migration_log = Logger('/var/tmp/uos-migration/migration.log', logging.DEBUG, logging.DEBUG)
 
@@ -78,15 +75,6 @@ def check_kernel(data):
     return 'success'
 
 
-def check_services(data, url):
-    info = post_client_data(data, url)
-    if info is None or info.status_code != 200:
-        print("请求失败,客户端没有启动")
-        return list_to_json(['res', 'error'], ['1', '迁移客户端没有启动'])
-    else:
-        return info.text
-
-
 def check_environment(data):
     """
     agent迁移前环境检查任务
@@ -105,35 +93,6 @@ def check_environment(data):
     return 'success'
 
 
-def check_os(data):
-    services = check_services(data, '/check_os')
-    if services:
-        return services
-
-
-def check_storage(data):
-    services = check_services(data, '/check_storage')
-    if services:
-        return services
-
-
-def close_tool(data):
-    os.system('kill -9 `ps -ef | grep "start_webview.py" | grep -v grep | awk \'{print $2}\'`')
-    data = {"ip": "0.0.0.0"}
-    data_json = json.dumps(data)
-    return data_json
-
-
-def check_user(data):
-    services = check_services(data, '/check_user')
-    if services:
-        json_data = json.loads(data)
-        with open('/usr/lib/migration-tools-server/.passwd.txt', 'w', encoding='utf-8') as f:
-            text = json_data['passwd']
-            f.write(text)
-        return services
-
-
 def check_repo(data):
     """
     检测平台软件仓库
@@ -147,36 +106,6 @@ def check_repo(data):
         DBHelper().execute(repo_status_sql)
     get_agent_ip(data, sql, '/check_repo')
     return 'success'
-
-
-def check_os_kernel(data):
-    services = check_services(data, '/check_os_kernel')
-    if services:
-        return services
-
-
-def check_repo_kernel(data):
-    services = check_services(data, '/check_repo_kernel')
-    if services:
-        return services
-
-
-def check_progress(data):
-    services = check_services(data, '/check_progress')
-    if services:
-        return services
-
-
-def check_migration_progress(data):
-    services = check_services(data, '/check_migration_progress')
-    if services:
-        return services
-
-
-def export_migration_reports(data):
-    services = check_services(data, '/export_migration_reports')
-    if services:
-        return services
 
 
 def system_migration(data):
@@ -195,9 +124,3 @@ def system_migration(data):
         json_data = json.dumps(data)
         send_task_to_agent(json_data, url, i.get('agent_ip'))
     return 'success'
-
-
-def migration_details(data):
-    services = check_services(data, '/migration_details')
-    if services:
-        return services
