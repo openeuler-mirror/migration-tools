f6aba64e611852243a222a808fec89fcebf20b74
wangluwei <wangluwei@uniontech.com>
2023-11-23 15:47:19 +0800

    add datenum function

--- templates/src/assets/excel/Export2Excel.js
+++ templates/src/assets/excel/Export2Excel.js
@@ -43,3 +43,8 @@ function generateArray(table) {
     return [out, ranges];
 };
 
+function datenum(v, date1904) {
+    if (date1904) v += 1462;
+    let epoch = Date.parse(v);
+    return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+}
