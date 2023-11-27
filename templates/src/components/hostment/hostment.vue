15f3e75cec132efd35b7d83cf84439340f3e6696
wangluwei <wangluwei@uniontech.com>
2023-11-27 15:00:23 +0800

    add hostment.vue el-card

---
+++ templates/src/components/hostment/hostment.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="head">
+      <div class="head-1"></div>
+      <span>主机管理</span>
+    </div>
+
+    <el-card>
+      <img src="./img/u189.png" alt="" />&ensp;
+      "主机管理"用于管理未完成迁移工作的任务
+    </el-card>
+  </div>
+</template>
+
+<style src="./index.css" scoped>
+
+</style>
