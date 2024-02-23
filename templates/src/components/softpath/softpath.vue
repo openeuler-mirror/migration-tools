973de6c7c4c9d46dafacda8775873ce7c9f3bf92
wangluwei <wangluwei@uniontech.com>
2024-02-23 17:05:54 +0800

    add softpath.vue template head

---
+++ templates/src/components/softpath/softpath.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+    <div class="head">
+      <div class="head-1"></div>
+      <span>软件仓库路径</span>
+    </div>
+    <el-card>
+      <img src="./img/u189.png" alt="" />
+      请根据一下提示设置软件仓库地址
+    </el-card>
+
+  </div>
+</template>
+
+
+
+<style src="./index.css" scoped>
+
+</style>
