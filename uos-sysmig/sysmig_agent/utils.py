421f3180e2372a0755a071d549a742f6e40c4092
xuezhixin <xuezhixin@uniontech.com>
2024-06-13 13:51:46 +0800

    add upload html report

--- uos-sysmig/sysmig_agent/utils.py
+++ uos-sysmig/sysmig_agent/utils.py
@@ -1,26 +1,44 @@
 # SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
 # SPDX-License-Identifier:   MulanPubL-2.0-or-later
-
-import json
-
-def list_to_json(keylist, valuelist):
-    res = dict(zip(keylist, valuelist))
-    res = json.dumps(res)
-    return json.dumps(res)
+import pymysql,os
+from connect_sql import DBHelper
+from logger import migration_log
+from sysmig_agent.migration import get_mig_state
+from sysmig_agent.share import run_subprocess, get_local_ip
 
 
-# 检测进度
-def messageProgress(message):
-    with open('/var/tmp/uos-migration/.progress','w') as fp:
-        fp.write(message)
-        fp.close()
+class DBupload(object):
+    """
+    Put reports into database based on folder contents.
+    """
+
+    def __init__(self, htmlpath):
+        super().__init__()
+        self.html_path = htmlpath
+
+    def read_html(self):
+        if not os.path.exists(self.html_path):
+            migration_log.error('Please check you report')
+            return False
+        with open(self.html_path, 'r') as ht:
+            print(self.html_path)
+            html = pymysql.escape_string(ht.read())
+            ht.close()
+        return html
+
+    def upload_html(self):
+        html = self.read_html()
+        if not html:
+            return False
+        report_type = self.html_path.split('.', -1)[len(self.html_path.split('.', -1)) - 1]
+        sql = "INSERT INTO report_info  ( agent_ip , report_type , report_name ,create_time, report_contect) VALUES(" \
+              "'{}','{}','{}',NOW(),'{}');".format(get_local_ip(), report_type, os.path.basename(self.html_path), html)
+        try:
+            ret = DBHelper().execute(sql)
+        except:
+            pass
 
 
-def messageState(message):
-    with open('/var/tmp/uos-migration/.state','w') as fp:
-        fp.write(message)
-
-fp.close()
 def selfDestruct(task_id):
     """
     destroy agent system migration rpm.
