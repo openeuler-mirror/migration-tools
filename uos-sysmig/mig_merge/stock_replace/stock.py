298edc19b5816a070600778ff69935d77365bce8
xuezhixin <xuezhixin@uniontech.com>
2024-06-14 10:44:29 +0800

    add basic systemd information.

--- uos-sysmig/mig_merge/stock_replace/stock.py
+++ uos-sysmig/mig_merge/stock_replace/stock.py
@@ -43,6 +43,53 @@ def rpmpkg_formatting(file_name):
     return format_info.rsplit(',',1)[0]+"]},"
 
 
+def get_system_info():
+    '''
+        应用场景：存量替换迁移检测-系统基本信息
+        功    能：1xxxa版系统基本信息，按照前后端接口生成json格式系统基本信息
+        输入参数：无
+        返 回 值：json数据
+    '''
+    line_num = 0
+
+    sysinfo_name = FixedPageInfo.inventory_data_dir + '/systeminfo.txt'
+    for line in open(sysinfo_name, 'r'):
+        line_num += 1
+
+        if line_num == 2:
+            middle_data = '"current_os_version": "'+line.strip().split('|')[1]+'",'
+            system_list = FixedPageInfo.page_system_info + middle_data
+
+        if line_num == 3:
+            middle_data = '"current_os_kennel_version": "'+line.strip().split('|')[1]+'",'
+            system_list = system_list + middle_data
+
+        if line_num == 5:
+            middle_data = '"var_cache_available_space": "'+line.strip().split('|')[1]+'",'
+            system_list = system_list + middle_data
+
+        if line_num == 6:
+            middle_data = '"architecture": "'+line.strip().split('|')[1]+'",'
+            system_list = system_list + middle_data
+
+        if line_num == 9:
+            middle_data = '"replaced_software_package_count": "'+line.strip().split('|')[1]+'",'
+            system_list = system_list + middle_data
+
+        if line_num == 12:
+            middle_data = '"compatible_software_package_count": "'+line.strip().split('|')[1]+'",'
+            system_list = system_list + middle_data
+
+        if line_num == 13:
+            middle_data = '"incompatible_software_package_count": "'+line.strip().split('|')[1]+'",'
+            system_list = system_list + middle_data
+
+        if line_num == 14:
+            middle_data = '"software_package_count": "'+line.strip().split('|')[1]+'"},'
+            system_list = system_list + middle_data
+
+    return system_list
+
 def get_softpkg_compatibility():
     '''
         应用场景：存量替换迁移检查-软件包对比
