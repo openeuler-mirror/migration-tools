bfb3dc7c7d57b736751c5613d3974e34326bac23
lixin <lixinb@uniontech.com>
2024-06-18 17:10:54 +0800

    modify FileDiffCard.vue

--- ui/report_templates/src/components/ec/FileDiffCard.vue
+++ ui/report_templates/src/components/ec/FileDiffCard.vue
@@ -4,7 +4,7 @@
     <div @click="toggleShow(item)" class="flexBox">
       <div class="leftIconBox">
         <img
-          src="@/assets/forward.svg"
+          src="data:image/svg+xml;utf8, <svg viewBox='0 0 1024 1024' xmlns='http://www.w3.org/2000/svg'><path fill='currentColor' d='M340.864 149.312a30.592 30.592 0 0 0 0 42.752L652.736 512 340.864 831.872a30.592 30.592 0 0 0 0 42.752 29.12 29.12 0 0 0 41.728 0L714.24 534.336a32 32 0 0 0 0-44.672L382.592 149.376a29.12 29.12 0 0 0-41.728 0z'/></svg>"
           :class="{
             arrowIcon: !item.show,
             arrowIconRotate: item.show,
@@ -16,9 +16,10 @@
         <div>{{ item.desc }}</div>
       </div>
       <div class="spacing"></div>
-      <div v-if="item.show">
-        <div v-html="item.diffHtml"></div>
-      </div>
+      <div class="right"></div>
+    </div>
+    <div v-if="item.show">
+      <div v-html="item.diffHtml"></div>
     </div>
   </div>
 </template>
@@ -36,7 +37,7 @@ export default {
     },
     items: Array,
   },
-  methos: {
+  methods: {
     toggleShow: function (item) {
       item.show = !item.show;
     },
