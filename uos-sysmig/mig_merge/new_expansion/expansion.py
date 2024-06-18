434e712cd141b29cb947c5a3f0764b848760521a
xuezhixin <xuezhixin@uniontech.com>
2024-06-18 09:41:32 +0800

    move from merge.py to expansion.py, add sysconf_diff function

--- uos-sysmig/mig_merge/new_expansion/expansion.py
+++ uos-sysmig/mig_merge/new_expansion/expansion.py
@@ -6,7 +6,7 @@ import sys
 import json
 import platform
 
-from config import FixedPageInfo
+from config import FixedInfo
 from new_expansion.scan_rpms import scan_rpms
 from migrationTools.scanHardware import utils
 from migrationTools.utils.config import PathConf
@@ -16,21 +16,38 @@ from migrationTools.exportSysConf.paramdata import ParamData
 class MigrationMerge:
 
     def __init__(self) -> None:
-        self.template_dir = FixedPageInfo.report_template_dir
-        self.report_dir = FixedPageInfo.report_dir
+        self.template_dir = FixedInfo.template_dir
+        self.report_expansion_dir = FixedInfo.report_expansion_dir
 
-        data = self.hardware_analysis()
-        data = data + self.conf_scan()
-        data = data + self.rpms_scan()
-        data = data + self.sysconf_diff()
+        #data = self.sysconf_diff()
+        #data = self.hardware_analysis()
+        data = self.conf_scan()
+        #data = data + self.rpms_scan()
+        print('data = %s' %(data))
+
+    def sysconf_diff(self):
+        '''系统配置差异导出
+            按照前后端接口，生成json格式数据，对应html报告tab页
+        '''
+        #PARAM_CONFIG_DIR = os.path.join(PathConf.data_path, "export-config")
+        #param_config_file = os.path.join(PARAM_CONFIG_DIR, "param_config.json")
+        #param_data = ParamData(param_config_file)
+        #param_data = FixedInfo.expansion_page_head + '{"confGroupName": "文件系统配置","confList": [{'
+        with open('json.txt', mode='r') as fp:
+            param_data = str(fp.readlines()).replace('\\\\n', '\\n')
+
+        print('param_data = %s' %(param_data))
+        return param_data
 
     def hardware_analysis(self):
         '''硬件兼容性分析
             按照前后端接口，生成json格式数据，对应html报告tab页
         '''
+
+        FixedInfo.hardware_tabs_head
         compatability_list = utils.get_compatability_list(
             utils.get_pci_list(), utils.get_supported_device_list(), False)
-        jsonstr = json.dumps(compatability_list)
+        jsonstr = FixedInfo.hardware_tabs_head + json.dumps(compatability_list)+'},'
         return jsonstr
 
     def conf_scan(self):
@@ -39,6 +56,9 @@ class MigrationMerge:
         '''
         data_01 = ScanConf.get_sysctl_a_data(self)
         data_02 = ScanConf.get_system_service_data(self)
+        print('data_01 = %s' %(data_01))
+        print('data_02 = %s' %(data_02))
+
         return str(data_01) + str(data_02)
 
     def rpms_scan(self):
@@ -46,16 +66,6 @@ class MigrationMerge:
             按照前后端接口，生成json格式数据，对应html报告tab页
         '''
         return scan_rpms()
-
-    def sysconf_diff(self):
-        '''系统配置差异导出
-            按照前后端接口，生成json格式数据，对应html报告tab页
-        '''
-        PARAM_CONFIG_DIR = os.path.join(PathConf.data_path, "export-config")
-        param_config_file = os.path.join(PARAM_CONFIG_DIR, "param_config.json")
-        param_data = ParamData(param_config_file)
-        print('param_data = %s' %(param_data))
-        return param_data
 
 def main():
     MigrationMerge()
