bbb73b1c08ba85a2d5cbc68c45c755a24b0ef022
xuezhixin <xuezhixin@uniontech.com>
2024-06-21 17:36:08 +0800

    get the pakcage version for generrate report

--- uos-sysmig/mig_merge/merge/utils.py
+++ uos-sysmig/mig_merge/merge/utils.py
@@ -1,4 +1,38 @@
+import os
 
+def is_number(s):
+    '''
+        应用场景：rpm包匹配
+        功    能：判断某个字符是否为数字
+        输入参数：s-字符变量
+        返 回 值：True-字符为数字；False-字符非数字
+    '''
+
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
+
+def gen_pkg_version(pkg):
+    key = len(pkg)+1
+    cmd = 'rpm -qa %s' %(pkg)
+    for line in os.popen(cmd):
+        if is_number(line.replace('\n', '')[key]):
+            rpm_info = line.replace('\n', '').rsplit('.', 1)[0].rsplit('-', 2)
+            version = rpm_info[1]+'-'+rpm_info[2]
+            return version
+
+    return False
 
 def dataplaceholder_replace(template, report, replace_str):
     '''按照前后端接口（json格式）替换检测报告中数据占位符 "dataPlaceholder"
