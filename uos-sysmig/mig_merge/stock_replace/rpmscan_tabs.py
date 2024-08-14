ac91009ab31879a4f9b2f8643fb887bb1cd60152
lihaipeng <lihaipeng@uniontech.com>
2024-08-14 03:45:39 -0400

    Fixed abnormal query of sqlite database

--- uos-sysmig/mig_merge/stock_replace/rpmscan_tabs.py
+++ uos-sysmig/mig_merge/stock_replace/rpmscan_tabs.py
@@ -38,9 +38,31 @@ def rpmscan_tabs(logger):
         return '-1'
 
     for pkg_name in installed_pkgs:
+        #多个sqlite查询，存在多个重复查询结果
+        #需要对插叙结果格式化，只输出唯一结果
+        #eg: gssproxy-0.8.0-15.el8.x86_64 在111.sqlite查询结果为空；在222.sqlite查询结果也为空，报告有两条结果
+        i = 0
+        tmp_pkg_info_list = []
+        other_provided_list = []
+        nothind_provided_list = []
+
         for sqlite in sqlite_list:
             tmp_pkg_info = ParsedPkgInfoAdd(pkg_name, sqlite, add_tags)
-            parsed_pkgs.append(tmp_pkg_info)
+            tmp_pkg_info_list.append(tmp_pkg_info)
+            tmp_info = str(tmp_pkg_info.tags)
+            if 'Nothing provided' in tmp_info:
+                nothind_provided_list.append(tmp_info+'|'+str(i))
+            else:
+                other_provided_list.append(tmp_info+'|'+str(i))
+            i += 1
+        #此时单个包对sqlite列表遍历结束，开始格式化输出结果
+        if len(sqlite_list) == len(nothind_provided_list):
+            #单个包对sqlite遍历，所有结果都为 'Nothing provided' 格式化输[0]
+            parsed_pkgs.append(tmp_pkg_info_list[0])
+        else:
+            #单个包对sqlite遍历，所有结果非 'Nothing provided' 格式化输出[0]
+            ele_num = other_provided_list[0].split('|')[1]
+            parsed_pkgs.append(tmp_pkg_info_list[int(ele_num)])
 
     only_show_leap = True
     if only_show_leap:
