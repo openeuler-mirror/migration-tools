37da8deb306cacb201281b9e8595bbd5466b1b12
lihaipeng <lihaipeng@uniontech.com>
2024-08-20 02:47:38 -0400

    Add bidding function to inspection process

--- uos-sysmig/mig_merge/stock_replace/sysconffile_tabs.py
+++ uos-sysmig/mig_merge/stock_replace/sysconffile_tabs.py
@@ -8,11 +8,14 @@ from mig_merge.config import FixedInfo
 from mig_merge.migrationTools.scanHardware import utils
 from mig_merge.migrationTools.exportSysConf.paramdata import ParamData
 from mig_merge.migrationTools.utils.config import PathConf
+from mig_merge.public_relations.public_relations_data import public_relations
 
 def sysconffile_tabs():
     '''硬件兼容性分析
         按照前后端接口，生成json格式数据，对应html报告tab页
     '''
+
+    pr_data = public_relations(FixedInfo.pr_cfg_file)
 
     sysconffile_tabs_json = '"sysconffile_tabs": {"name": "SysSconf","data": '
 
@@ -20,7 +23,7 @@ def sysconffile_tabs():
     param_config_file = os.path.join(PARAM_CONFIG_DIR, "param_config.json")
     param_data = ParamData(param_config_file)
 
-    sysconf_str = sysconffile_tabs_json + str(json.dumps(param_data.return_data)) + '}'
+    sysconf_str = sysconffile_tabs_json + str(json.dumps(pr_data.return_data + param_data.return_data)) + '}'
     return sysconf_str
 
 def main():
