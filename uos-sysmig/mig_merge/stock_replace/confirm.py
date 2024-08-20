a6631a655ade5c990fb231372c2cad61dd5bbf35
lihaipeng <lihaipeng@uniontech.com>
2024-08-20 02:31:49 -0400

    Check the existence of the agent system release file

--- uos-sysmig/mig_merge/stock_replace/confirm.py
+++ uos-sysmig/mig_merge/stock_replace/confirm.py
@@ -58,6 +58,11 @@ def get_cur_sys_version():
         输入参数：无
         返 回 值：CentOS Linux 8 (Core)
     '''
+
+    if not os.path.exists('/etc/os-release'):
+        print("Please CHECK /etc/os-release WHETHER EXIT!!!")
+        return "-1"
+
     fp = open('/etc/os-release', 'r')
     for line in fp:
         if 'PRETTY_NAME' in line:
